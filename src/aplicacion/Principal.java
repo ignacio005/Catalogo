3e57273d0280b453939848de16c5e428641c34e0
ignacio005 <ignacio.tiradomeza@usp.ceu.es>
2023-12-08 15:25:51 +0100

    Cambio

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -2,6 +2,7 @@ package aplicacion;
 
 import dominio.Catalogo;
 import dominio.Coche;
+import dominio.enun.*;
 import presentacion.Interfaz;
 
 import java.math.BigDecimal;
@@ -12,11 +13,27 @@ public class Principal {
         Catalogo catalogo = new Catalogo();
 
         Coche coche1 = new Coche("BMW", "X5", new BigDecimal(86800));
-        Coche coche2 = new Coche("Ford", "Explorer", new BigDecimal(91638.1));
-        Coche coche3 = new Coche("Toyota", "Rav4", new BigDecimal(40100));
-
+        coche1.setnuevaRueda(Rueda.GRANDE);
+        coche1.setnuevoCombustible(Combustible.HIBRIDO);
+        coche1.setnuevoColor(Color.NEGRO);
+        coche1.setnuevosAsientos(Asientos.CUERO);
+        coche1.setnuevoTipoDeCambio(Tipodecambio.MANUAL);
         catalogo.annadirCoche(coche1);
+
+        Coche coche2 = new Coche("Ford", "Explorer", new BigDecimal(91638.1));
+        coche2.setnuevaRueda(Rueda.GRANDE);
+        coche2.setnuevoCombustible(Combustible.DIESEL);
+        coche2.setnuevoColor(Color.ROJO);
+        coche2.setnuevosAsientos(Asientos.CUERO);
+        coche2.setnuevoTipoDeCambio(Tipodecambio.AUTOMATICO);
         catalogo.annadirCoche(coche2);
+
+        Coche coche3 = new Coche("Toyota", "Rav4", new BigDecimal(40100));
+        coche3.setnuevaRueda(Rueda.PEQUENO);
+        coche3.setnuevoCombustible(Combustible.ELECTRICO);
+        coche3.setnuevoColor(Color.AZUL);
+        coche3.setnuevosAsientos(Asientos.TELA);
+        coche3.setnuevoTipoDeCambio(Tipodecambio.MANUAL);
         catalogo.annadirCoche(coche3);
 
         Interfaz.MostrarInstrucciones();
